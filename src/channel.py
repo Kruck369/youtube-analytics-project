8ad73a950c7ced05be9c7b62b35dc35198780363
Kruck <reka_931@mail.ru>
2023-04-24 09:23:41 +0300

    Реализован класс Channel с выводом информации по id канала.

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,8 @@
+import os
+import json
+
+from googleapiclient.discovery import build
+CHANNEL_INFO = 'channel_info.json'
 
 
 class Channel:
@@ -5,8 +10,26 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        api_key: str = os.getenv('YOUTUBE_API_KEY')
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        with open(CHANNEL_INFO, 'a', encoding='utf-8') as f:
+            if os.stat(CHANNEL_INFO).st_size == 0:
+                json.dump([channel], f, ensure_ascii=False)
+            else:
+                with open(CHANNEL_INFO, encoding='utf-8') as f:
+                    channel_list = json.load(f)
+                    for i in range(0,):
+                        if self.channel_id not in channel_list[i]['items']:
+                            pass
+                        else:
+                            channel_list.append(channel)
+                with open(CHANNEL_INFO, 'w', encoding='utf-8') as f:
+                    json.dump(channel_list, f, ensure_ascii=False)
+        print(channel)
+
+
