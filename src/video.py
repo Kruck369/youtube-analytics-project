e2038424910d3ead55cc5a9b8a733eda938ff768
Kruck <reka_931@mail.ru>
2023-05-15 16:09:00 +0300

    Доработка с учетом условий домашнего задания 6.

--- src/video.py
+++ src/video.py
@@ -1,7 +1,7 @@
 import os
-import json
 
 from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
 api_key: str = os.getenv('YOUTUBE_API_KEY')
 youtube = build('youtube', 'v3', developerKey=api_key)
@@ -10,14 +10,26 @@ youtube = build('youtube', 'v3', developerKey=api_key)
 class Video:
     def __init__(self, video_id):
         self.video_id = video_id
-        video = youtube.videos().list(id=self.video_id, part='snippet,statistics').execute()
-        self.video_title = video["items"][0]['snippet']["title"]
-        self.video_url = f"https://www.youtube.com/watch?v={self.video_id}"
-        self.video_views = video['items'][0]['statistics']['viewCount']
-        self.video_likes = video['items'][0]['statistics']['likeCount']
+        try:
+            video = youtube.videos().list(id=self.video_id, part='snippet,statistics').execute()
+            if len(video["items"]) > 0:
+                self.title = video["items"][0]['snippet']["title"]
+                self.video_url = f"https://www.youtube.com/watch?v={self.video_id}"
+                self.video_views = video['items'][0]['statistics']['viewCount']
+                self.like_count = video['items'][0]['statistics']['likeCount']
+            else:
+                self.title = None
+                self.video_url = None
+                self.video_views = None
+                self.like_count = None
+        except HttpError:
+            self.title = None
+            self.video_url = None
+            self.video_views = None
+            self.like_count = None
 
     def __str__(self):
-        return f"{self.video_title}"
+        return f"{self.title}"
 
 
 class PLVideo(Video):
